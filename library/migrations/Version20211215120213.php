204eb20c8dedef9848e9cdcf9ffff4a7abc7f716
DoritoDog <kareembelgharbi@gmail.com>
2021-12-15 14:22:47 +0100

    Fix links

--- library/migrations/Version20211215120213.php
+++ library/migrations/Version20211215120213.php
@@ -21,6 +21,12 @@ final class Version20211215120213 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category ADD directory VARCHAR(255) NOT NULL');
+
+        $this->addSql('UPDATE category SET directory = \'Pocitacova_grafika\' WHERE id = 1');
+        $this->addSql('UPDATE category SET directory = \'Operacne_systemy\' WHERE id = 2');
+        $this->addSql('UPDATE category SET directory = \'Databazove_systemy\' WHERE id = 3');
+        $this->addSql('UPDATE category SET directory = \'Pocitacova_analyza_dat\' WHERE id = 4');
+        $this->addSql('UPDATE category SET directory = \'Multimedia\' WHERE id = 5');
     }
 
     public function down(Schema $schema): void
